aec36ea56b09c8b2b36ed6bfac078caa85a33dd5
shamsce <shamsbasir@gmail.com>
2017-07-26 12:59:07 +0300

    New one-could not delete the old files

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+
+
 $(function () {
+    
     // set the konva stage in div with id 'canvas'
-    var width = 500;
+    var width =500;
     var height = 500;
     var stage = new Konva.Stage({
         container: 'canvas',
         width: width,
         height: height
     });
+
     var layer = new Konva.Layer();
     stage.add(layer);
 
@@ -14,11 +18,28 @@ $(function () {
     var drawLine = function (x1, y1, x2, y2) {
         console.log("Drawing Line:", x1, y1, x2, y2);
         var line = new Konva.Line({
-            points: [x1, -y1 + height, x2, -y2 + height],
+            points: [x1+width/2, -y1+height/2, x2+width/2, -y2+height/2],
             stroke: 'red',
+            strokeWidth: 4,
+            fill:'#00ffcd',
+             lineCap: 'round',
+             lineJoin: 'round'
+          
+    
+        });
+        layer.add(line);
+        layer.draw();
+    };
+    //function to add meshes
+     var drawMesh = function (x1, y1, x2, y2) {
+        console.log("Drawing Line:", x1, y1, x2, y2);
+        var line = new Konva.Line({
+            points: [x1+width/2, -y1+height/2, x2+width/2, -y2+height/2],
+            stroke: 'blue',
             strokeWidth: 1,
-            lineCap: 'round',
-            lineJoin: 'round'
+            fill:'#00ffcd'
+          
+    
         });
         layer.add(line);
         layer.draw();
@@ -26,6 +47,8 @@ $(function () {
 
     // global coordinate container
     var coordinates = [];
+    var xvalues=[];
+    var yvalues=[];
 
     // handle submits on form with id 'drawForm'
     $("#drawForm").submit(function (event) {
@@ -40,6 +63,8 @@ $(function () {
         coordinate = coordinate.split(",");
         var x = parseFloat(coordinate[0]);
         var y = parseFloat(coordinate[1]);
+        xvalues.push(x);
+        yvalues.push(y);
 
         // if there is some kind of invalid input, warn the user and return
         if (isNaN(x) || isNaN(y)) {
@@ -55,7 +80,6 @@ $(function () {
             $("#coordinates").append("Loop closed.");
             var lastPoint = coordinates[coordinates.length - 1];
             drawLine(lastPoint[0], lastPoint[1], x, y);
-            
             return;
         }
 
@@ -76,5 +100,66 @@ $(function () {
         drawLine(lastPoint[0], lastPoint[1], newPoint[0], newPoint[1]);
     });
     
+   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    //meshing is here: 
+    
+    $('#mesh').click(function(){
+      var segment=$("#segment").val();
+      $("#segment").val("");
+        
+      mesafe=parseInt(segment);
+        
+      var xMax=Math.max.apply(null,xvalues);
+      var xMin=Math.min.apply(null,xvalues);
+      var yMax=Math.max.apply(null,yvalues);
+      var yMin=Math.min.apply(null,yvalues);
+      var deltaX=Math.abs(xMax-xMin)/mesafe;
+      var deltaY=Math.abs(yMax-yMin)/mesafe;
+        
+        if(coordinates.length===3){
+            
+            console.log('under construction')
+        }
+        
+        if(coordinates.length ===4){
+            x1=xMin;
+            y1=yMin;
+            for(i=1;i<mesafe+1;i++){
+                for (j=1;j<mesafe;j++){
+                
+                drawMesh(x1,y1+deltaY,x1+deltaX,y1+deltaY);
+                y1+=deltaY;
+                }
+                x1+=deltaX;
+                y1=yMin;
+              
+            }
+            
+            //===============================
+            x1=xMin;
+                for(i=1;i<mesafe+1;i++){
+                for (j=1;j<mesafe;j++){
+                
+                drawMesh(x1+deltaX,y1,x1+deltaX,y1+deltaY);
+                x1+=deltaX;
+                }
+                y1+=deltaY;
+                x1=xMin;
+              
+            }    
+        }    
+    });
+ 
+    
+  // I could not find a method to remove the canvas content and keep drawing again??
+  // anyway I tried it clears the content but does not draw unless the page is refreshed. 
+            $("#delete").click(function(){
+                coordinates=[];
+                $('#coordinates').html('');
+ 
+                });
+    
 });
+
+
 
